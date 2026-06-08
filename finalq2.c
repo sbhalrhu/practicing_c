f1644e31bfc858db5b08b3ed99e3e91ab5d207dd
sbhalrhu <sahibbhalrhu@gmail.com>
2026-06-07 23:31:59 -0700

    got rid of unused vars

--- finalq2.c
+++ finalq2.c
@@ -8,14 +8,11 @@ int main(int argc, char *argv[]) {
 
 int extractbs(char *bitstring) {
     int count=0;
-    char *buf[8];
     for (int i=1; bitstring[i] != '\0'; i++) {
-        buf[i] = bitstring[i];
         if (bitstring[i] == '1') {
             count=count+1;
         }
     }
-    buf[7] = '\0';
     if ((count %2) == 0) { return (0x7F & (int)bitstring); }
     return EOF;
 
