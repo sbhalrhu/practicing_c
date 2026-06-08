59fe93ae96a555ace0c2df8d814fbee247c331b7
sbhalrhu <sahibbhalrhu@gmail.com>
2026-06-08 00:48:11 -0700

    implemented provided solution

--- finalq2.c
+++ finalq2.c
@@ -1,16 +1,25 @@
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     int bitstring = extractbs(argv[1]);
-    printf("Bitstring is %d.", (int)bitstring);
 
+    if (argc < 2) {
+        printf("Usage: ./finalq2 <bitstring>\n");
+        return EXIT_FAILURE;
+
+    }
+
+    printf("Bitstring is %d.", (int)bitstring);
+    return EXIT_SUCCESS;
 }
 
-int extractbs(char *bitstring) {
+char extractbs(unsigned char bitstring) { /*unsigned char is 1 byte -- 8 bits*/
     int count=0;
-    for (int i=1; bitstring[i] != '\0'; i++) {
-        if (bitstring[i] == '1') {
+    unsigned char mask;
+    for (mask = 1; mask != 0; mask <<=1) {
+        if (bitstring & mask) {
             count=count+1;
         }
     }
